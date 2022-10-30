7546f4b1422757ad0bdd22f78b8a82d27cf3e4b7
chengyu <chengyu_chung@qidianwuxiandeMacBook-Air.local>
2022-10-30 12:25:53 +0800

    Transaction reverted: function returned an unexpected amount of data

--- contracts/TestErc20.sol
+++ contracts/TestErc20.sol
@@ -4,5 +4,7 @@ pragma solidity ^0.8.10;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestErc20 is ERC20 {
-    constructor() ERC20("MyToken", "MTK") {}
+    constructor(string memory name, string memory symbol, uint supply) ERC20(name, symbol) {
+        _mint(msg.sender, supply);
+    }
 }
--- test/test.js
+++ test/test.js
@@ -3,22 +3,21 @@ const { ethers } = require("hardhat");
 
 describe("CERC20", function() {
     it("Should be able to mint or redeem", async function() {
+        //prepare owner and user account
+        const [owner, user1, user2] = await ethers.getSigners();
+
         //部署comptroller 
         const comptrollerFactory = await ethers.getContractFactory("Comptroller");
         const comptroller = await comptrollerFactory.deploy();
         await comptroller.deployed();
         console.log(`comptroller deployed to ${comptroller.address}`);
-            
-          
 
-
-        //部署erc20 
+        //部署erc20 token
         const erc20Factory = await ethers.getContractFactory("TestErc20");
-        const erc20 = await erc20Factory.deploy();
+        const erc20 = await erc20Factory.deploy("MyToken", "MTK", ethers.utils.parseUnits("10000", 18));
         await erc20.deployed();
         console.log(`erc20 deployed to ${erc20.address}`);
             
-         
 
         //部署interestRateModel_
         //找合約 參數先設為0 後部署
@@ -31,8 +30,7 @@ describe("CERC20", function() {
         await interestRateModel.deployed();
         console.log(`interestRateModel deployed to ${interestRateModel.address}`);
             
-    
-       
+
         //最後部署cErc20 
         const cErc20Factory = await ethers.getContractFactory("CErc20");
         const cErc20 = await cErc20Factory.deploy();
@@ -52,11 +50,10 @@ describe("CERC20", function() {
             comptroller.address,
             interestRateModel.address,
             ethers.utils.parseUnits("1", 18),//1:1
-            "MyToken",
-            "MTK",
+            "CompoundToken",
+            "CTK",
             18
         )
-      
 
         //部署simplePriceOracle
         const PriceOracleFactory = await ethers.getContractFactory("SimplePriceOracle");
@@ -67,7 +64,27 @@ describe("CERC20", function() {
         //設定priceOracle
         comptroller._setPriceOracle(priceOracle);
 
-        //W11 2. 部署完後mint 測試mintFresh
+        //2. 部署完後測試mint / redeem
+        //User1 使用 100 顆（100 * 10^18） ERC20 去 mint 出 100 CErc20 token，
+        //再用 100 CErc20 token redeem 回 100 顆 ERC20
+         //transfer erc20token to user1
+         await erc20.connect(owner).transfer(user1.address, ethers.utils.parseUnits("100",18));
+         console.log("transfered 100MTK to user1");
+         
+         //approve user1
+        await erc20.connect(user1).approve(cErc20.address, ethers.utils.parseUnits("100",18));
+        console.log("approved user1 interact with cErc20");
+
+        // add supportmarket
+        await comptroller.connect(owner)._supportMarket(cErc20.address);
+        console.log ("cErc20 added to comptroller market list");
+
         
+
+        //碰到Transaction reverted: function returned an unexpected amount of data
+        
+        await cErc20.connect(user1).mint(ethers.utils.parseUnits("100", 18));
+        console.log("user1 minted 100 CTK with 100 MTK!!");
+
     })
 })
